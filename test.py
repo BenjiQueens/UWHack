f242eaedd0f2bc67944469f4f86a87ed3458db01
Benji <benjichristie@hotmail.com>
2018-11-25 08:50:02 -0500

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,27 @@
-print("hello world")
+#### Benji Christie ####
+
+# UW Hackathon Test File
+import qrcode
+import qrcode.image.svg
+import qrtools
+from qrtools import QR
+
+# qr = pyqrcode.create("something this that")
+# qr.png("horn.png", scale = 6)
+
+qr = qrtools.QR()
+qr.decode("QR.jpg")
+print(qr.data)
+
+print("test")
+
+#
+
+if method == 'basic':
+    factory = qrcode.image.svg.SvgImage
+elif method == 'fragment':
+    factory = qrcode.image.svg.SvgFragmentImage
+else:
+    factory = qrcode.image.svg.SvgPathImage
+
+img = qrcode.make('data', image_factory=factory)
